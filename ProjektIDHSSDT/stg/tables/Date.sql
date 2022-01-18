f8b718356167cfe9e809c1b72b5f010b7cb1150e
Mateusz Więckowski <mr.mateuszwieckowski@gmail.com>
2022-01-18 19:43:59 +0100

    fix Date table, connection manager

--- ProjektIDHSSDT/stg/tables/Date.sql
+++ ProjektIDHSSDT/stg/tables/Date.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE stg.Date (
     Date date NOT NULL PRIMARY KEY,
     MonthNameEng varchar(50) NOT NULL,
+    Day numeric(2) NOT NULL,
     Month numeric(2) NOT NULL,
     Year numeric(4) NOT NULL,
 )
--- ProjektIDHSSDT/stg/tables/SalesOrder.sql
+++ ProjektIDHSSDT/stg/tables/SalesOrder.sql
@@ -2,11 +2,11 @@
 (
 	[idSalesOrder] INT NOT NULL PRIMARY KEY,
 	[idCustomer] INT NOT NULL,
-	[Date] date NOT NULL,
 	[idProduct] INT NOT NULL,
 	[idTerritory] INT NOT NULL,
 	[idSpecialOffer] INT NOT NULL,
 	[idStatus] INT NOT NULL,
+	[Date] date NOT NULL,
 	[TotalDue] MONEY NOT NULL,
 	[OrderQty] INT NOT NULL,
 
