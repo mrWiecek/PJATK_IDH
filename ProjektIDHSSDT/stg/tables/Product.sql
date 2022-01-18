bbee3c1eb2ad7fdddcc616835445236b6a904090
Mateusz Więckowski <mr.mateuszwieckowski@gmail.com>
2022-01-18 20:36:00 +0100

    stageProduct przeliczony z sukcesem

--- ProjektIDHSSDT/stg/tables/Product.sql
+++ ProjektIDHSSDT/stg/tables/Product.sql
@@ -2,7 +2,7 @@
 (
 	[idProduct] INT NOT NULL PRIMARY KEY,
 	[productName] NVARCHAR(50) NOT NULL,
-	[modelName] NVARCHAR(50) NOT NULL,
-	[subCategoryName] NVARCHAR(50) NOT NULL,
-	[categoryName] NVARCHAR(50) NOT NULL
+	[modelName] NVARCHAR(50) NULL,
+	[subCategoryName] NVARCHAR(50) NULL,
+	[categoryName] NVARCHAR(50) NULL
 )
