381e6d704c982fca0852703a9c348f52c4a2f09b
Mateusz Więckowski <mr.mateuszwieckowski@gmail.com>
2022-01-19 18:04:56 +0100

    Sales Order load

--- ProjektIDHSSDT/stg/tables/SalesOrder.sql
+++ ProjektIDHSSDT/stg/tables/SalesOrder.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [stg].[SalesOrder]
 (
-	[idSalesOrder] INT NOT NULL PRIMARY KEY,
+	[idSalesOrder] INT NOT NULL,
 	[idCustomer] INT NOT NULL,
 	[idProduct] INT NOT NULL,
 	[idTerritory] INT NOT NULL,
@@ -9,5 +9,14 @@
 	[Date] date NOT NULL,
 	[TotalDue] MONEY NOT NULL,
 	[OrderQty] INT NOT NULL,
+	CONSTRAINT [PK_SaleOrders] PRIMARY KEY (
+        [idSalesOrder],
+        [idCustomer],
+		[idProduct],
+		[idTerritory],
+		[idSpecialOffer],
+		[idStatus],
+		[Date]
+    )
 
 )
