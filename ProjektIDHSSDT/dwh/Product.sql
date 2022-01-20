c9807bcaaf4aefbc7fa8899f1e67ca805b9de83d
mmironczuk1 <s23303@pjwstk.edu.pl>
2022-01-20 13:18:42 +0100

    added dwh.SalesOrder, dwh.Product, dwh.SalesTerritory, dwh.Status,

---
+++ ProjektIDHSSDT/dwh/Product.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dwh].[Product]
+(
+	[idProduct_dwh] INT IDENTITY(1,1) PRIMARY KEY,
+	[idProduct] INT NOT NULL,
+	[productName] NVARCHAR(50) NOT NULL,
+	[modelName] NVARCHAR(50) NULL,
+	[subCategoryName] NVARCHAR(50) NULL,
+	[categoryName] NVARCHAR(50) NULL
+
+)
---
+++ ProjektIDHSSDT/dwh/Status.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dwh].[Status]
+(
+	[idStatus_dwh] INT IDENTITY(1,1) PRIMARY KEY,
+	[idStatus] INT NOT NULL,
+	[orderstatus] INT NOT NULL
+)
