11a615c22da5d9e4b3c0ac5fec670c7394ebff08
mmironczuk1 <s23303@pjwstk.edu.pl>
2022-01-21 10:10:20 +0100

    Zmiana SalesOrder, SpecialOffer

---
+++ ProjektIDHSSDT/dwh/SpecialOffer.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dwh].[SpecialOffer]
+(
+	[idSpecialOffer_dwh] INT IDENTITY(1,1) PRIMARY KEY,
+	[idProductSpecialOffer] INT NOT NULL,
+	[DiscountPercent] SMALLMONEY NOT NULL,
+
+)
--- ProjektIDHSSDT/stg/tables/SalesOrder.sql
+++ ProjektIDHSSDT/stg/tables/SalesOrder.sql
@@ -1,21 +1,15 @@
 ﻿CREATE TABLE [stg].[SalesOrder]
 (
-	[idSalesOrder] INT NOT NULL,
 	[idCustomer] INT NOT NULL,
 	[idProduct] INT NOT NULL,
 	[idTerritory] INT NOT NULL,
 	[idStatus] INT NOT NULL,
-	[UnitPrice] [money] NOT NULL,
-	[UnitPriceDiscount] [money] NOT NULL,
-	[SubTotal] [money] NOT NULL,
-	[TaxAmt] [money] NOT NULL,
-	[Freight] [money] NOT NULL,
 	[Date] date NOT NULL,
-	[TotalDue] MONEY NOT NULL,
+	[idSpecialOffer] SMALLMONEY NOT NULL,
+	[OrderValue] MONEY NOT NULL, 
 	[OrderQty] INT NOT NULL,
-	CONSTRAINT [PK_SaleOrders] PRIMARY KEY (
-        [idSalesOrder],
-        [idCustomer],
+	CONSTRAINT [PK_SalesOrders] PRIMARY KEY ( 
+		[idCustomer],
 		[idProduct],
 		[idTerritory],
 		[idStatus],
